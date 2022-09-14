402345580a553c2ae92b908bd8f56b8c2c4ad4f6
howset <howsetya@googlemail.com>
2022-09-14 16:00:11 +0200

    Fixed permissions on shell exercises

---
+++ shell01/ex08_add_chelou.sh
@@ -0,0 +1,9 @@
+# Make test
+# str1="\'?"
+# str2='"\"'
+# str3="'"
+# str4='\'
+# FT_NBR1=$str1$str2$str3$str4
+# FT_NBR2="rcrdmddd"
+
+echo $FT_NBR1 + $FT_NBR2 | sed "s/'/0/g" | tr '\\"?!' 1234 | tr mrdoc 01234 | xargs echo 'obase=13; ibase=5;' | bc | tr 0123456789ABC 'gtaio luSnemf'
