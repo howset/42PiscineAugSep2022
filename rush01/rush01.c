4fc31de377e89d71fa60cf2efc5f7546f35dad6c
Howard Setyamukti <showard@uni-potsdam.de>
2022-09-16 16:05:13 +0200

    Just trying to print the solution array

--- rush01/rush01.c
+++ rush01/rush01.c
@@ -10,3 +10,59 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int **cr_sol(void)
+{
+    int **sol;
+    int i;
+    int j;
+    int arr[4][4] =     {1,2,3,4,
+                        2,3,4,1,
+                        3,4,1,2,
+                        4,1,2,3};
+    
+    sol = (int**)malloc(sizeof(int*)*4);
+    i = 0;
+    while (i < 4)
+    {
+        sol[i] = (int*)malloc(sizeof(int)*4);
+        j = 0;
+        while (j < 4)
+        {
+            sol[i][j] = arr[i][j];
+            j++;
+        }
+        i++;
+    }
+    return (sol);
+}
+void pr_sol(int **sol)
+{
+    int i;
+    int j;
+
+    while (i < 4)
+    {
+        j = 0;
+        while (j < 4)
+        {
+            printf("%d", sol[i][j]);
+            printf("pr_sol");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
+
+int main(void)
+{
+    int **sol;
+
+    sol = cr_sol();
+    pr_sol(sol);
+    free(sol);
+    return (0);
+}
